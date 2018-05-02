7f8d12e4f562c0e740ee6030625156b95d21e06b
Smit Ray <humancoder@outlook.com>
2018-05-02 15:46:50 +0530

    Start server

--- src/client/pages/auth/test/_id.vue
+++ src/client/pages/auth/test/_id.vue
@@ -1,5 +1,14 @@
 <template>
-  <section>
-    hi
+  <section class="container container--full container--wr container--rel container--ovh addCourse">
+    <!-- Page banner -->
+    <page-banner>
+      Take</br>
+      Test
+    </page-banner>
+    <!-- Page banner end -->
+    <div class="container container--wr container--ovh container--rel container--block__ct addCourse--wr">
+      <router-link :to="{ name: 'auth' }"><i class="icon-"></i>Back to dashboard</router-link>
+
+    </div>
   </section>
 </template>
