23ef6080205f779686c029d8e47c39ef0772ebf6
Smit Ray <humancoder@outlook.com>
2018-05-02 16:05:22 +0530

    Plugin fixed

--- src/client/plugins/axios.js
+++ src/client/plugins/axios.js
@@ -1,10 +1,15 @@
-export default ({ $axios, store }) => {
+import jsCookie from 'cookie';
+
+export default ({ $axios, store, req }) => {
+  let token;
   if (process.server) {
-    return;
+    const cookie = jsCookie.parse(req.headers.cookie);
+    token = cookie.token; //eslint-disable-line
+  } else {
+    token = store.state.user.token; //eslint-disable-line
   }
 
   $axios.interceptors.request.use((request) => {
-    const { token } = store.state.user;
     if (token) {
       request.headers.common.Authorization = token;
     }
